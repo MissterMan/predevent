a503d7bbc36bdada6695a2a5c87251386c46680f
Pratama Putra Prayogi <putrayogi4545@gmail.com>
2022-09-05 12:25:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "maven-regular": ["Maven Pro Regular", "sans-serif"],
+        "maven-medium": ["Maven Pro Medium", "sans-serif"],
+        "maven-semiBold": ["Maven Pro SemiBold", "sans-serif"],
+        "maven-bold": ["Maven Pro Bold", "sans-serif"],
+        "maven-extraBold": ["Maven Pro ExtraBold", "sans-serif"],
+        "maven-black": ["Maven Pro Black", "sans-serif"],
+      },
+      colors: {
+        primary: "#006633",
+        secondary: "#FFCC00",
+      },
+    },
+  },
+  plugins: [],
+};
